649011c5b1c057d668fed29b2c9e330dc08adc0b
lebeaubm <lebeaubm@yahoo.com>
2024-06-24 12:38:15 -0700

    Update functions.php
    Custom post type

--- functions.php
+++ functions.php
@@ -30,3 +30,36 @@ function load_js()
 add_action('wp_enqueue_scripts', 'load_js');
 
 //Theme options
+
+
+function my_first_post_type()
+{
+
+	$args = array(
+
+
+		'labels' => array(
+
+					'name' => 'Cars',
+					'singular_name' => 'Car',
+		),
+		'hierarchical' => true,  //makes it like pages
+		'public' => true,
+		'has_archive' => true,
+		'menu_icon' => 'dashicons-images-alt2',
+		'supports' => array('title', 'editor', 'thumbnail','custom-fields'),
+		//'rewrite' => array('slug' => 'cars'),	
+
+	);
+
+
+	register_post_type('cars', $args);
+
+
+}
+add_action('init', 'my_first_post_type');
+
+
+
+
+
