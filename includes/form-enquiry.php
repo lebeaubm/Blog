47836fcc928eb763b9bedc90f1dca07de7b33e08
lebeaubm <lebeaubm@yahoo.com>
2024-06-24 13:34:31 -0700

    Update form-enquiry.php

--- includes/form-enquiry.php
+++ includes/form-enquiry.php
@@ -1,15 +1,15 @@
 <div id="success_message" class="alert alert-success" style="display:none"></div>
 
-
+<!-- Form for sending an enquiry about a car -->
 <form id="enquiry">
-//the title should be name of car
+<!-- Display the title of the car -->
     <h2>Send an enquiry about <?php the_title();?></h2>
 
+<!-- Hidden input to store the car's registration value -->
+<input type="hidden" name="registration" value="<?php the_field('registration');?>">
 
-    <input type="hidden" name="registration" value="<?php the_field('registration');?>">
 
-
-
+<!-- Form group for the first and last name -->
     <div class="form-group row">
     
             <div class="col-lg-6">
@@ -23,7 +23,7 @@
     </div>
 
 
-
+<!-- Form group for the email and phone number -->
     <div class="form-group row">
     
             <div class="col-lg-6">
@@ -37,14 +37,14 @@
     </div>
 
 
-
+<!-- Form group for the enquiry message -->
     <div class="form-group">
     
             <textarea name="enquiry" class="form-control" placeholder="Your Enquiry" required></textarea>
     </div>
 
 
-    
+<!-- Submit button -->
     <div class="form-group">
            <button type="submit" class="btn btn-success btn-block">Send your enquiry</button>
     </div>
@@ -52,31 +52,30 @@
 </form>
 
 
-
+<!-- JavaScript to handle form submission using jQuery -->
 <script>
 
 
 (function($){
-
-
+ // When the form is submitted
 
     $('#enquiry').submit( function(event){
 
 
         event.preventDefault();
-
+        // Set the AJAX endpoint URL
         var endpoint = '<?php echo admin_url('admin-ajax.php');?>';
-
+        // Serialize the form data
         var form = $('#enquiry').serialize();
-
+        // Create a new FormData object
         var formdata = new FormData;
-
+        // Append action, nonce, and form data to the FormData object
         formdata.append('action','enquiry');
         formdata.append('nonce', '<?php echo wp_create_nonce('ajax-nonce');?>');
         formdata.append('enquiry', form);
 
         
-
+        // Perform the AJAX request
         $.ajax(endpoint, {
 
             type:'POST',
@@ -84,7 +83,7 @@
             processData: false,
             contentType: false,
   
-
+            // On success
             success: function(res){
 
                     $('#enquiry').fadeOut(200);
@@ -99,9 +98,10 @@
 
             },
 
-
+            // On error
             error: function(err)
             {
+                // Display an error alert with the error message
                 alert(err.responseJSON.data);
 
             }
